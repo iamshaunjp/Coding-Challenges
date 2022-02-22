a3a959ccc3895449939c7eb502f96f56e25b44b7
Wei <linkano.teck@gmail.com>
2022-02-23 02:23:12 +1100

    deploy overlay

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 ;(function () {
 	const triggerButton = document.querySelector('.triggerIcon')
+	const slideOverlay = document.querySelector('.slideOverlay')
 
 	function handleMenuStateChange() {
 		const lines = Array.from(document.querySelectorAll('.line'))
@@ -7,7 +8,13 @@
 
 		lines.forEach((line) => line.classList.toggle('active'))
 		slideMenu.classList.toggle('active')
+		slideOverlay.classList.toggle('active')
 	}
 
 	triggerButton.addEventListener('click', handleMenuStateChange)
+	slideOverlay.addEventListener('mouseover', handleMenuStateChange)
+
+	document.addEventListener('click', (e) => {
+		console.log(e.target)
+	})
 })()
