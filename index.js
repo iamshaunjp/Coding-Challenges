7686a28cfe8c752e93336e47ee4c438de5c3fd9e
Wei <linkano.teck@gmail.com>
2022-02-26 02:39:45 +1100

    challenge-2 complete v.2

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 ;(function () {
 	const email = document.querySelector('#email')
-	const tick = document.querySelector('.tick')
+	const field = document.querySelector('.field')
 	const checkReg = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
 
 	function handleTextChange(e) {
 		const isPass = checkReg.test(e.target.value)
-
-		email.classList.toggle('ok', isPass)
-		tick.classList.toggle('ok', isPass)
+		field.classList.toggle('ok', isPass)
 	}
 
 	email.addEventListener('input', handleTextChange)
